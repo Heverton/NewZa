601fe33a196809c978ca5c6e7d0477aa4cb40846
heverton da silva cruz <heverton7@gmail.com>
2020-09-04 19:47:58 -0300

    #7 ajustado a estruturado do projeto

---
+++ src/app/imovel/imovel.ts
@@ -0,0 +1,11 @@
+export class Imovel {
+    nome: string;
+    descricao: string;
+    valor: string;
+
+    constructor(nome, descricao, valor){
+        this.nome = nome;
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+}
---
+++ src/app/inquilino/Inquilino.ts
@@ -0,0 +1,12 @@
+export class Inquilino {
+    nome: string;
+    telefone: string;
+    sexo: string;
+    dtNascimento: string;
+    estadocivil: string;
+    isPossuiFilhos: string;
+    isPossuiWhatsapp: string;
+    nomeConjuge: string;
+    sexoConjuge: string;
+    dtNascimentoConjuge: string;
+}
