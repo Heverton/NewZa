8611f441247082187a2c9c28b10d3b922a57ba80
heverton da silva cruz <heverton7@gmail.com>
2020-10-07 18:03:43 -0300

    Implementaçao do Authenticated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuardService } from './shared/auth/auth-guard.service';
+import { RoleGuardService } from './shared/auth/role-guard.service';
 
 const routes: Routes = [
   {
@@ -8,8 +11,17 @@ const routes: Routes = [
   },
   {
     path: 'sis',
-    loadChildren: () => import('./abas/abas.module').then(m => m.AbasComponentModule)
+    loadChildren: () => import('./abas/abas.module').then(m => m.AbasComponentModule),
+    canActivate: [AuthGuardService]
   },
+  {
+    path: 'admin', // Teste com Role
+    component: LoginComponent,
+    canActivate: [RoleGuardService],
+    data: {
+      expectedRole: 'admin'
+    }
+  }
 ];
 @NgModule({
   imports: [
