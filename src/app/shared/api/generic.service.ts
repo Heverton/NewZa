22d5ec8b6cfafe376402d95af17795e5c7fd335d
heverton da silva cruz <heverton7@gmail.com>
2020-11-02 21:37:36 -0300

    Implementaçao do Authenticated

--- src/app/shared/api/generic.service.ts
+++ src/app/shared/api/generic.service.ts
@@ -9,28 +9,28 @@ import { environment } from 'src/environments/environment';
  */
 @Injectable()
 export abstract class GenericService<T> {
-    private _header = new HttpHeaders({ 'Content-Type': 'application/json' });
-    private _api = environment.endpoints.api;
+    private header = new HttpHeaders({ 'Content-Type': 'application/json' });
+    private api = environment.endpoints.api;
 
-    constructor(protected _http: HttpClient, protected _base: string){ }
+    constructor(protected http: HttpClient, protected base: string){ }
 
     public inserir(dados: T): Observable<T[]> {
         const body = JSON.stringify(dados);
-        return this._http.post<T[]>(`${this._api}/${this._base}`, body, {headers: this._header });
+        return this.http.post<T[]>(`${this.api}/${this.base}`, body, {headers: this.header });
     }
 
     public editar(dados: T): Observable<T[]> {
         const body = JSON.stringify(dados);
-        return this._http.put<T[]>(`${this._api}/${this._base}`, body, {headers: this._header });
+        return this.http.put<T[]>(`${this.api}/${this.base}`, body, {headers: this.header });
     }
 
     public excluir(dados: T): Observable<T[]> {
         // TODO verificar
         const body = JSON.stringify(dados);
-        return this._http.patch<T[]>(`${this._api}/${this._base}`, body, {headers: this._header });
+        return this.http.patch<T[]>(`${this.api}/${this.base}`, body, {headers: this.header });
     }
 
     public buscar(): Observable<T[]> {
-        return this._http.get<T[]>(`${this._api}/${this._base}`, {headers: this._header });
+        return this.http.get<T[]>(`${this.api}/${this.base}`, {headers: this.header });
     }
 }
